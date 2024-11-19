5970735357550619d23ef04e8f91851f64aa0f4b
Tamás Mikola <73854810+TamasMikola@users.noreply.github.com>
2024-11-19 21:03:43 +0100

    8.2.1 (#5)

--- src/RGF.Blazor.RgfApexCharts/Configuration.cs
+++ src/RGF.Blazor.RgfApexCharts/Configuration.cs
@@ -10,13 +10,12 @@ namespace Recrovit.RecroGridFramework.Blazor.RgfApexCharts;
 
 public class RgfApexChartsConfiguration
 {
-    public static async Task LoadResourcesAsync(IJSRuntime jsRuntime, bool shouldLoadBundledStyles = true)
+    public static async Task LoadResourcesAsync(IJSRuntime jsRuntime)
     {
         var libName = Assembly.GetExecutingAssembly().GetName().Name;
-        if (shouldLoadBundledStyles)
-        {
-            await jsRuntime.InvokeVoidAsync("Recrovit.LPUtils.AddStyleSheetLink", $"{RgfClientConfiguration.AppRootPath}_content/{libName}/{libName}.bundle.scp.css", false, RgfApexChartsCssLib);
-        }
+        await jsRuntime.InvokeVoidAsync("Recrovit.LPUtils.EnsureStyleSheetLoaded", "rgf-check-stylesheet-rgfcharts", "<div class=\"rgf-check-stylesheet-rgfcharts\" rgf-apexcharts=\"\">",
+            $"{RgfClientConfiguration.AppRootPath}_content/{libName}/{libName}.bundle.scp.css?v={FileVersion}", RgfApexChartsCssLib, "Recrovit.RecroGridFramework.Client.Blazor.bundle.scp.css");
+
         await jsRuntime.InvokeAsync<IJSObjectReference>("import", $"{RgfClientConfiguration.AppRootPath}_content/{libName}/scripts/" +
 #if DEBUG
             "recrovit-rgf-apexcharts.js"
@@ -35,20 +34,21 @@ public class RgfApexChartsConfiguration
     private static readonly string RgfApexChartsCssLib = "rgf-apexcharts-lib";
 
     public static readonly string JsApexChartsNamespace = "Recrovit.RGF.Blazor.ApexCharts";
+
+    public static string FileVersion => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()!.Version;
 }
 
 public static class RgfApexChartsConfigurationExtension
 {
-    public static async Task InitializeRGFBlazorApexChartsAsync(this IServiceProvider serviceProvider, bool loadResources = true, bool shouldLoadBundledStyles = true)
+    public static async Task InitializeRGFBlazorApexChartsAsync(this IServiceProvider serviceProvider, bool loadResources = true)
     {
         RgfBlazorConfiguration.RegisterComponent<ChartComponent>(RgfBlazorConfiguration.ComponentType.Chart);
         if (loadResources)
         {
             var jsRuntime = serviceProvider.GetRequiredService<IJSRuntime>();
-            await RgfApexChartsConfiguration.LoadResourcesAsync(jsRuntime, shouldLoadBundledStyles);
+            await RgfApexChartsConfiguration.LoadResourcesAsync(jsRuntime);
         }
-        var ver = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
         var logger = serviceProvider.GetRequiredService<ILogger<RgfApexChartsConfiguration>>();
-        logger?.LogInformation($"RecroGrid Framework Blazor ApexCharts v{ver} initialized.");
+        logger?.LogInformation("RecroGrid Framework Blazor ApexCharts v{Version} initialized.", RgfApexChartsConfiguration.FileVersion);
     }
 }
