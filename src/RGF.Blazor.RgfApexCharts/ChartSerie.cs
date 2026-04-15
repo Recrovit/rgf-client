2f0a2831267299d8b09a2d6ec20a834198b40743
Tamás Mikola <mikola.tamas@recrovit.hu>
2026-04-15 09:53:54 +0200

    RGF.Blazor.RgfApexCharts v10.0.0

---
+++ src/RGF.Blazor.RgfApexCharts/ChartSerie.cs
@@ -0,0 +1,15 @@
+﻿namespace Recrovit.RecroGridFramework.Blazor.RgfApexCharts;
+
+public class ChartSerie
+{
+    public string? Name { get; set; }
+
+    public List<ChartSerieData> Data { get; set; } = [];
+}
+
+public class ChartSerieData
+{
+    public object? X { get; set; }
+
+    public decimal Y { get; set; }
+}
