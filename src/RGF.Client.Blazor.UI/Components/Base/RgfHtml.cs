edf39ddc6bf005412b0b4c7906c578fa33c93966
Tamás Mikola <mikola.tamas@recrovit.hu>
2026-04-15 09:53:07 +0200

    RGF.Client.Blazor.UI v10.0.0

---
+++ src/RGF.Client.Blazor.UI/Components/Base/RgfHtml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Recrovit.RecroGridFramework.Client.Blazor.UI.Components.Base;
+
+public class RgfHtml : RgfBaseComponent
+{
+    [Parameter, EditorRequired]
+    public string TagName { get; set; } = default!;
+
+    [Parameter]
+    public object? RenderKey { get; set; }
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = null!;
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnDblClick { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnContextMenu { get; set; }
+
+    protected RenderFragment? Content { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        base.BuildRenderTree(builder);
+
+        if (string.IsNullOrEmpty(TagName))
+        {
+            return;
+        }
+
+        int sequence = 0;
+        builder.OpenElement(sequence++, TagName);
+        if (RenderKey != null)
+        {
+            builder.SetKey(RenderKey);
+        }
+
+        var attributes = Attributes;
+        if (attributes != null && attributes.Count > 0)
+        {
+            foreach (var attribute in attributes)
+            {
+                builder.AddAttribute(sequence++, attribute.Key, attribute.Value);
+            }
+        }
+
+        if (OnClick.HasDelegate)
+        {
+            builder.AddAttribute(sequence++, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnClick));
+        }
+
+        if (OnDblClick.HasDelegate)
+        {
+            builder.AddAttribute(sequence++, "ondblclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnDblClick));
+        }
+
+        if (OnContextMenu.HasDelegate)
+        {
+            builder.AddAttribute(sequence++, "oncontextmenu", EventCallback.Factory.Create<MouseEventArgs>(this, OnContextMenu));
+        }
+
+        builder.AddElementReferenceCapture(sequence++, capturedRef => _elementReference = capturedRef);
+
+        if (ChildContent != null)
+        {
+            builder.AddContent(sequence++, ChildContent);
+        }
+
+        builder.CloseElement();
+    }
+}
---
+++ src/RGF.Client.Blazor.UI/Components/TreeNodeComponent.razor.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
+using Recrovit.RecroGridFramework.Abstraction.Contracts.Services;
+using Recrovit.RecroGridFramework.Abstraction.Models;
+using Recrovit.RecroGridFramework.Client.Blazor.Parameters;
+using Recrovit.RecroGridFramework.Client.Events;
+using Recrovit.RecroGridFramework.Client.Handlers;
+using System.Text;
+
+namespace Recrovit.RecroGridFramework.Client.Blazor.UI.Components;
+
+public partial class TreeNodeComponent : IDisposable
+{
+    [Inject]
+    private ILogger<TreeNodeComponent> _logger { get; set; } = default!;
+
+    [Inject]
+    private IRecroDictService RecroDict { get; set; } = default!;
+
+    private RgfEntityParameters? EntityParameters => Node.EntityParameters;
+
+    private IRgManager Manager => TreeComponent.Manager;
+
+    public async override Task SetParametersAsync(ParameterView parameters)
+    {
+        await base.SetParametersAsync(parameters);
+
+        Node.TooltipText = null;
+
+        if (EntityParameters != null)
+        {
+            var eventArg = new RgfEventArgs<RgfTreeEventArgs>(this, new RgfTreeEventArgs(RgfTreeEventKind.NodeParametersSet, this, Node));
+            await EntityParameters.TreeParameters.EventDispatcher.DispatchEventAsync(eventArg.Args.EventKind, eventArg);
+
+            if (Node.TooltipText != null)
+            {
+                return;
+            }
+        }
+
+        if (Node.Property != null && Node.Property.ListType != PropertyListType.RecroGrid)
+        {
+            StringBuilder str = new();
+            str.Append("<div class='rgf-tree-tooltip'>");
+            foreach (var prop in Manager.ListHandler.SortedVisibleColumns)
+            {
+                var data = Node.RowData.GetItemData(prop.Alias).StringValue;
+                str.Append("<div class='row justify-content-start align-items-center'>")
+                .AppendFormat("<div class='rgf-tooltip-title col-3 text-end text-truncate'>{0}</div>", prop.ColTitle)
+                .Append("<div class='col-auto'>:</div>")
+                .AppendFormat("<div class='rgf-tooltip-data col text-start text-truncate'>{0}</div>", data)
+                .Append("</div>");
+            }
+            str.Append("</div>");
+            Node.TooltipText = str.ToString();
+        }
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+        if (firstRender)
+        {
+            EntityParameters?.EventDispatcher.Subscribe(RgfEntityEventKind.Initialized, OnEntityInitialized);
+        }
+    }
+
+    private void OnEntityInitialized(IRgfEventArgs<RgfEntityEventArgs> args)
+    {
+        StateHasChanged();
+    }
+
+    private void OnEmbedded(MouseEventArgs arg)
+    {
+        if (EntityParameters != null)
+        {
+            EntityParameters.DisplayMode = EntityParameters.DisplayMode == RfgDisplayMode.Tree ? RfgDisplayMode.Grid : RfgDisplayMode.Tree;
+        }
+    }
+
+    public void Dispose()
+    {
+        EntityParameters?.UnsubscribeFromAll(this);
+    }
+    private async Task OnRefresh(MouseEventArgs args)
+    {
+        if (EntityParameters?.Manager != null)
+        {
+            var eventArgs = new RgfEventArgs<RgfToolbarEventArgs>(this, new RgfToolbarEventArgs(RgfToolbarEventKind.Refresh, null));
+            await EntityParameters.ToolbarParameters.EventDispatcher.DispatchEventAsync(eventArgs.Args.EventKind, eventArgs);
+        }
+    }
+}
